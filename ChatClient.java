65d0862bf59cd88455d269e2d184472a4ac03ea1
Prachi Rajput <prachiraj9149@gmail.com>
2025-02-17 17:29:43 +0530

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -1,9 +1,11 @@
+// Importing all the necessary libraries required for the project
 import java.io.*;
 import java.net.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+// Defining class
 public class ChatClient {
     private Socket socket;
     private BufferedReader in;
@@ -12,10 +14,15 @@ public class ChatClient {
     private JTextArea textArea;
     private JTextField textField;
 
+
+    // A constructor that conects to the server and generates GUI.
     public ChatClient(){
         buildGUI();
         connectToServer();
     }
+
+
+    // Building GUI for client.
     private void buildGUI(){
         frame = new JFrame("Chat Applicaion");
         textArea = new JTextArea();
@@ -30,6 +37,8 @@ public class ChatClient {
         frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        // Adding Event listener to send message
         textField.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
                 sendMessage(textField.getText());
@@ -37,12 +46,16 @@ public class ChatClient {
             }
         });
     }
+
+
+    //Following code connects the client to the chat server.
     private void connectToServer(){
         try{
             socket = new Socket("localhost", 1234);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
 
+            // Starting thread 
             new Thread(new IncomingReader()).start();
 
         } catch(IOException e){
